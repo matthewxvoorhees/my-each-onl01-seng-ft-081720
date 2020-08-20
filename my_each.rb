714eaf224b8f1ca6b13b5235bcda510373ef569a
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-20 21:41:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,3 +5,10 @@ while test < array.length
   test + 1
   end
 end
+
+You have you create an variable that will keep count the number of loop. Like
+Count=0
+While count < array.length
+(here you pass in the yield command with an argument that is the current element of the array )
+yield ( array [count])
+Count +=1
